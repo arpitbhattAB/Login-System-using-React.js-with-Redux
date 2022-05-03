3e50bfa2c13e90f08d038a6c8d7dfd508d29af4f
ARPIT BHATT <86553891+arpitbhattAB@users.noreply.github.com>
2022-05-03 11:26:18 +0530

    Add files via upload

--- features/userSlice.js
+++ features/userSlice.js
@@ -1,1 +1,21 @@
+import { createSlice } from "@reduxjs/toolkit";
 
+export const userSlice = createSlice({
+  name: "user",
+  initialState: {
+    user: undefined,
+  },
+  reducers: {
+    login: (state, action) => {
+      state.user = action.payload;
+    },
+    logout: (state) => {
+      state.user = undefined;
+    },
+  },
+});
+
+export const userSliceActions = userSlice.actions;
+
+
+export default userSlice.reducer;
